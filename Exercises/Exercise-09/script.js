70c54a5f4985fc29c1c3da473fd82d19b79f5a04
Zcyyyyyy <chenyu@cca.edu>
2022-05-07 10:22:50 -0700

    e9

---
+++ Exercises/Exercise-09/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.basketball').draggable();
+    $('.baseball').draggable();
+    $('.bedminton').draggable();
+    $('.football').draggable();
+    $('.softball').draggable();
+});
